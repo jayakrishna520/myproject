9c2ccf30ffd1b3176ef9a2e549081df0eca5657e
jayakrishna520 <36194914+jayakrishna520@users.noreply.github.com>
2018-02-22 18:23:46 +0530

    Update MyprojectApplication.java
    Updated Application start up class to support external tomcat deployment

--- myproject/src/main/java/io/javabrains/MyprojectApplication.java
+++ myproject/src/main/java/io/javabrains/MyprojectApplication.java
@@ -2,10 +2,16 @@ package io.javabrains;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class MyprojectApplication {
-
+public class MyprojectApplication extends SpringBootServletInitializer {
+    
+    @Override
+    protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+        return application.sources(MyprojectApplication.class);
+    }
 	public static void main(String[] args) { 
 		SpringApplication.run(MyprojectApplication.class, args);
 	}
